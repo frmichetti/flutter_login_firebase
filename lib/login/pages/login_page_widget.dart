f676b72675770ce9983051fcfa6469d26017103f
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2021-01-23 12:49:09 -0300

    finished login page

--- lib/login/pages/login_page_widget.dart
+++ lib/login/pages/login_page_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_firebase/login/widgets/circle_button.dart';
 
 class LoginPageWidget extends StatefulWidget {
   @override
@@ -8,6 +9,45 @@ class LoginPageWidget extends StatefulWidget {
 class _LoginPageWidgetState extends State<LoginPageWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40),
+      color: Theme.of(context).accentColor.withOpacity(0.9),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            height: 60,
+          ),
+          Icon(Icons.headset_mic, size: 40, color: Colors.white),
+          Container(
+            height: 25,
+          ),
+          RichText(
+              text: TextSpan(
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                  children: <TextSpan>[
+                TextSpan(text: "Awesome"),
+                TextSpan(
+                    text: "App", style: TextStyle(fontWeight: FontWeight.bold))
+              ])),
+          Container(
+            height: 120,
+          ),
+          CircleButton(
+            label: "SIGN UP",
+            backgroundColor: Colors.transparent,
+            borderColor: Colors.white,
+          ),
+          Container(
+            height: 30,
+          ),
+          CircleButton(
+            label: "LOGIN",
+            backgroundColor: Colors.white,
+            textColor: Theme.of(context).accentColor,
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/login/widgets/circle_button.dart
+++ lib/login/widgets/circle_button.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 class CircleButton extends StatelessWidget {
   final String label;
+  final Color backgroundColor;
+  final Color textColor;
+  final Color borderColor;
 
-  const CircleButton({Key key, this.label = ""}) : super(key: key);
+  const CircleButton ({Key key, this.label = "", this.backgroundColor, this.textColor = Colors.white, this.borderColor}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +14,14 @@ class CircleButton extends StatelessWidget {
       width: double.infinity,
       height: 60,
       decoration: BoxDecoration(
-        color: Theme.of(context).accentColor,
+        color: backgroundColor == null ? Theme.of(context).accentColor : backgroundColor,
         borderRadius: BorderRadius.circular(40),
+        border: borderColor == null ? null : Border.all(color: borderColor)
       ),
       alignment: Alignment.center,
       child: Text(
         label,
-        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        style: TextStyle(color: textColor, fontWeight: FontWeight.bold),
       ),
     );
   }
